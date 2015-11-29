f534628b88fd995de727f98ac2492c48c60fe03a
Mark Gibson <mgibson@adaptavist.com>
2015-11-29 21:02:44 +0000

    use map of attrs

--- js/jsx-h.js
+++ js/jsx-h.js
@@ -4,18 +4,23 @@ export default function(tag, props, ...children) {
   return h(tag, transformProps(props), children)
 }
 
+const attrMap = {
+  "class": "className"
+}
+
 function transformProps(props) {
   if (props) {
-    if (props.class) {
-      props.className = props.class
-      delete props.class
-    }
-
     Object.getOwnPropertyNames(props).forEach(key => {
-      if (key.startsWith("data-")) {
+      const sub = attrMap[key]
+      if (sub) {
+        props[sub] = props[key]
+        delete props[key]
+
+      } else if (key.startsWith("data-")) {
         props.dataset = props.dataset || {}
         props.dataset[key.substr(5)] = props[key]
         delete props[key]
+
       } else if (key.indexOf("-") !== -1) {
         props.attributes = props.attributes || {}
         props.attributes[key] = props[key]
